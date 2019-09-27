8123c672c760617f6bd570c9da7a973d016fb53e
generous-calculator-4715 <andrew.lewell@gmail.com>
2019-09-27 22:22:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 function takeANumber(line, name) {
-  var number = line.length + 1;
-  
   line.push(name);
-  
-  return `Welcome, ${name}. You are number ${number} in line.`;
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length < 1) {
+var lineNumber = 0
+
+function takeANumber(line) {
+  lineNumber++;
+  line.push(lineNumber);
+  return `Welcome, you have ticket number ${lineNumber}.`;
+}
+
+function nowServing(line) {
+  if (line.length < 1) {
     return "There is nobody waiting to be served!";
   } else {
-    return "Currently serving " + katzDeliLine.shift() + ".";
+    return `Currently serving ${line.shift()}.`;
   }
 }
 
@@ -20,10 +25,11 @@ function currentLine(line) {
   if (line.length < 1) {
     return "The line is currently empty."; 
   } else {
-    for (var i = 0; i < line.length; ++i) {
+    for (var i = 0; i < line.length; i++) {
       str = str + ` ${i+1}. ${line[i]},`;
     }
   }
+  
   str = str.substring(0, str.length -1);
   return str;
 }
